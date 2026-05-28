3e07b145920c39b06e07889230c5cea3b3ea0753
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-05-28 15:58:57 -0300

    feat: Add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import { Text, View } from "react-native";
+import { styles } from "./Style";
+
+export default function Footer() {
+  return <Text style={styles.text}>Desenvolvido por Leonardo Behnck :)</Text>;
+}
